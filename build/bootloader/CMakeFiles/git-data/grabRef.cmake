2c54820dc0ef7fb7984c3d3336b574e4032db643
omar <39051822+km@users.noreply.github.com>
2025-09-18 20:08:43 +0200

    helper functions

--- build/bootloader/CMakeFiles/git-data/grabRef.cmake
+++ build/bootloader/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/omar/Desktop/Retail-Analytics-ESP32/build/bootloader/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Espressif/frameworks/esp-idf-v5.5.1/.git")
+set(GIT_DIR "C:/Users/omar/esp/v5.5.1/esp-idf/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- main/main.cpp
+++ main/main.cpp
@@ -17,10 +17,52 @@
 
 #include "dl_image_jpeg.hpp"
 #include "pedestrian_detect.hpp"
+#include <stdlib.h>
+//marker line to figure out if user entered or exited
+#define LineY 80
+
+//Range in which centroid assumes its the same pedestrian
+#define samePedestrianX 10
+#define samePedestrianY 15
+
+typedef struct {
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    int centroidX;
+    int centroidY;
+} Pedestrian;
+
+
+//calculate centroid for x or y depending on input
+void calculateCentroid(Pedestrian* p1)
+{
+    int cx = (p1->x1 + p1->x2)/2;
+    int cy = (p1->y1 + p1->y2)/2;
+    p1->centroidX = cx;
+    p1->centroidY = cy;
+}
+
+
+//check if pedestrian is same as last frame
+//This is achieved by seeing if the centroids are closer enough especially on the X axis
+bool samePedestrian(Pedestrian p1, Pedestrian p2)
+{  
+    int diffX = p1.centroidX - p2.centroidX;
+    int diffY = p1.centroidY - p2.centroidY;
+
+
+    //check if between ranges
+    return (abs(diffX) <= samePedestrianX) && (abs(diffY) <= samePedestrianY);
+}
+
+ 
 
 
 
 extern "C" void app_main(void)
 {
-    // your code here
+    
 }
