0f67167c25239ee71bac5c689c769adf052572aa
omar <39051822+km@users.noreply.github.com>
2025-09-18 14:26:18 +0200

    upd

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-void app_main(void)
+#include "esp_camera.h"
+#include "esp_event.h"
+#include "esp_heap_caps.h"
+#include "esp_http_server.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_netif_ip_addr.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+#include "esp_psram.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "dl_image_jpeg.hpp"
+#include "pedestrian_detect.hpp"
+
+
+
+extern "C" void app_main(void)
 {
-
+    // your code here
 }
